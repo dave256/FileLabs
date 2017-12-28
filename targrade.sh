79f99decb24f3f43d39e3453f53eb4de5e6d5c3b
Dave Reed <dave256@mac.com>
2017-12-27 23:27:10 -0500

    add targrade.sh

---
+++ targrade.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+find . | grep part|grep -E "(html|ksh|ATT.*\.htm)" | xargs /bin/rm
+find . | grep __MACOSX | xargs /bin/rm -rf
+find . | grep -E "ATT.*\.htm" | xargs /bin/rm
+find . | grep -E "ATT.*\.txt" | xargs /bin/rm
+
+cd ..
+tar zcvf Grade.tar.gz Grade
+cd Grade
