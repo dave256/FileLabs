6fdb8d9274a420e26c356d60bb536b761cda30dd
Dave Reed <dave256@mac.com>
2018-02-12 19:44:49 -0500

    targrade.sh - cd into Grade directory first if not already in it

--- targrade.sh
+++ targrade.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -d "Grade" ]; then
+    cd Grade
+fi
+
 find . | grep part|grep -E "(html|ksh|ATT.*\.htm)" | xargs /bin/rm
 find . | grep __MACOSX | xargs /bin/rm -rf
 find . | grep -E "ATT.*\.htm" | xargs /bin/rm
