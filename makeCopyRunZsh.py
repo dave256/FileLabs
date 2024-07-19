0f3b1097da474b40588082a28a9e78190973fe3f
Dave Reed <dave256@mac.com>
2024-07-19 19:18:09 -0400

    makeCopyRunZsh.py hand Swift Tests files
    copy files with Tests in the name to the Swift Package test directory following the standard naming conventions

--- makeCopyRunZsh.py
+++ makeCopyRunZsh.py
@@ -61,11 +61,32 @@ def main():
         if lastSlash != -1:
             f = f[lastSlash + 1:]
         files.append(f)
+
+    # copy source files and test files (and file with "Tests" as part of filename)
+    sourceFiles = list(filter(lambda f: not ("Tests" in f), files))
+    testFiles = list(filter(lambda f: ("Tests" in f), files))
+
     joinedFiles = " ".join(files)
+    sourceFiles = " ".join(sourceFiles)
+    testFiles = " ".join(testFiles)
+
     runLines = []
     if not args.skip:
         runLines.append(f'rmmain.py {joinedFiles}')
-    runLines.append(f'echo -e "/bin/cp -f {joinedFiles} {args.dest}" > copy.zsh')
+
+    # remove extra spaces and make certain it ends in a slash
+    dest = args.dest.strip()
+    if dest[-1] != "/":
+        dest += "/"
+
+    if len(testFiles) > 0:
+        # change to location where Swift package has its Tests files
+        testDest = dest.replace("/Sources/", "/Tests/")
+        testDest = testDest[:-1] + "Tests/"
+        runLines.append(f'echo -e "/bin/cp -f {sourceFiles} {dest}; /bin/cp -f {testFiles} {testDest}" > copy.zsh')
+    else:
+        runLines.append(f'echo -e "/bin/cp -f {sourceFiles} {dest}" > copy.zsh')
+
     runLines.append("chmod 755 copy.zsh")
     runLines = "\n".join(runLines)
 
